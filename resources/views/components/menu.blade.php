b9ab54095e40f94f6265e0bbfd5b4b88e59acb24
FikryAS <azizfikry15@gmail.com>
2021-08-06 18:59:45 +0700

    menggunakan methode isActive pada menu blade untuk diimplementasikan

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="nav flex-column">
     @foreach($list AS $row)
-    <a class="nav-link" href="#">
+    <a class="nav-link {{ $isActive($row['label']) ? 'active' : '' }}" href="#">
         {{ $row['label'] }}
     </a>
     @endforeach
