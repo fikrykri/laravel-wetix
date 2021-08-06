dfc0d417096d6c56b343679ac0c0825eaf705663
FikryAS <azizfikry15@gmail.com>
2021-08-06 18:58:50 +0700

    memberikan variabel active menjadi Dashboard

--- app/Http/Controllers/dashboard/DashboardController.php
+++ app/Http/Controllers/dashboard/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $active = 'Aku Aktif';
+        $active = 'Dashboard';
 
         return view('home', ['active' => $active]);
     }
