2f32b6ecd07e09c91d798956c0866a3300c29c6a
FikryAS <azizfikry15@gmail.com>
2021-08-10 01:41:03 +0700

    memperbaiki tampilan form

--- resources/views/dashboard/user/form.blade.php
+++ resources/views/dashboard/user/form.blade.php
@@ -4,7 +4,7 @@
 <div class="card">
   <div class="card-header">
     <div class="row">
-      <div class="col-8">
+      <div class="col-8 align-self-center">
         <h3>Users</h3>
       </div>
       <div class="col-4 text-right">
@@ -15,7 +15,7 @@
     </div>
   </div>
 
-  <div class="card-body p-0">
+  <div class="card-body">
     <div class="row">
       <div class="col-md-8 offset-md-2">
         <form action="{{ url('dashboard/user/update/'. $user->id) }}" method="post">
@@ -35,7 +35,7 @@
             <span class="text-danger">{{ $message }}</span>
             @enderror
           </div>
-          <div class="form-group">
+          <div class="form-group mb-0">
             <button type="submit" class="btn btn-success btn-sm">Update</button>
           </div>
         </form>
