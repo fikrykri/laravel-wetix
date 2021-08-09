ee7ab1513d0b02370b538d592b9c1afed022d525
FikryAS <azizfikry15@gmail.com>
2021-08-10 02:26:23 +0700

    merubah standar penamaan global

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::get('/dashboard', [App\Http\Controllers\Dashboard\DashboardController::cl
 
 // Users
 Route::get('/dashboard/users', [App\Http\Controllers\Dashboard\UserController::class, 'index'])->name('dashboard.users');
-Route::get('/dashboard/user/edit/{id}', [App\Http\Controllers\Dashboard\UserController::class, 'edit'])->name('dashboard.user.edit');
-Route::put('/dashboard/user/update/{id}', [App\Http\Controllers\Dashboard\UserController::class, 'update'])->name('dashboard.user.update');
-Route::delete('/dashboard/user/delete/{id}', [App\Http\Controllers\Dashboard\UserController::class, 'destroy'])->name('dashboard.user.delete');
+Route::get('/dashboard/users/{id}', [App\Http\Controllers\Dashboard\UserController::class, 'edit'])->name('dashboard.user.edit');
+Route::put('/dashboard/users/{id}', [App\Http\Controllers\Dashboard\UserController::class, 'update'])->name('dashboard.user.update');
+Route::delete('/dashboard/users/{id}', [App\Http\Controllers\Dashboard\UserController::class, 'destroy'])->name('dashboard.user.delete');
